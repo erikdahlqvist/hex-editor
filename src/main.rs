a6acef561daf60dd607424ecd807757175a7a643
Erik Dahlkvist <erikdahlqvist@protonmail.com>
2024-12-23 15:09:41 +0100

    Added status bar

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{env, fs};
 
 use crossterm::event::{self, Event, KeyCode, KeyEvent};
-use ratatui::{style::{Color, Stylize}, text::{Line, Span}, widgets::{Block, Paragraph, Wrap}, Frame};
+use ratatui::{layout::{Constraint, Direction, Layout}, style::{Color, Stylize}, text::{Line, Span}, widgets::{Block, Paragraph, Wrap}, Frame};
 
 fn main() {
     let file_path = get_file_path();    
@@ -12,10 +12,10 @@ fn main() {
     let mut bytes = parse_bytes(raw_bytes);
     let mut selected_byte = 0;
 
-    let mut input_buffer = String::new();
+    let mut input_buffer = String::with_capacity(2);
 
     loop {
-        terminal.draw(|frame| draw(frame, &bytes, selected_byte)).unwrap();
+        terminal.draw(|frame| draw(frame, &bytes, selected_byte, &input_buffer)).unwrap();
         match event::read().unwrap() {
             Event::Key(KeyEvent {code ,..}) => {
                 match code {
@@ -30,15 +30,13 @@ fn main() {
                         input_buffer.remove(input_buffer.len() - 1);
                     }
                     KeyCode::Char('w') => save(file_path.clone(), bytes.clone()),
-                    KeyCode::Char(c) => {
-                        if c.is_ascii_hexdigit() {
-                            input_buffer += &c.to_uppercase().collect::<String>();
-                            if input_buffer.len() >= 2 {
-                                bytes[selected_byte] = input_buffer.clone();
-                                input_buffer.clear();
-                            }
-                        }
+                    KeyCode::Char(c) => if c.is_ascii_hexdigit() && input_buffer.len() < 2 {
+                        input_buffer += &c.to_uppercase().collect::<String>();
                     },
+                    KeyCode::Enter => if input_buffer.len() >= 2 {
+                        bytes[selected_byte] = input_buffer.clone();
+                        input_buffer.clear();
+                    }
                     _ => (),
                 }
             }
@@ -75,9 +73,9 @@ fn parse_bytes(bytes: Vec<u8>) -> Vec<String> {
     return bytes.iter().map(|byte| format!("{byte:X}")).collect();
 }
 
-fn draw(frame: &mut Frame, bytes: &Vec<String>, selected_byte: usize) {
-    let spans = bytes.iter().enumerate().flat_map(|(i, byte)|
-            vec![
+fn draw(frame: &mut Frame, bytes: &Vec<String>, selected_byte: usize, input_buffer: &str) {
+    let editor_span = bytes.iter().enumerate().flat_map(|(i, byte)|
+            [
                 Span::from(byte).bg(if i == selected_byte {
                     Color::DarkGray
                 } else {
@@ -87,8 +85,23 @@ fn draw(frame: &mut Frame, bytes: &Vec<String>, selected_byte: usize) {
             ]
         ).collect::<Vec<Span>>();
 
-    let text: Line = spans.into();
+    let editor_text: Line = editor_span.into();
+    let editor = Paragraph::new(editor_text)
+        .wrap(Wrap {trim: true})
+        .block(Block::bordered());
 
-    let editor = Paragraph::new(text).wrap(Wrap {trim: true}).block(Block::bordered());
-    frame.render_widget(editor, frame.area());
+    let main_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(Vec::from([Constraint::Percentage(100), Constraint::Min(1)]))
+        .split(frame.area());
+
+    let status_bar_layout= Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(Vec::from([Constraint::Length(20), Constraint::Length(20)]))
+        .split(main_layout[1]);
+
+    frame.render_widget(editor, main_layout[0]);
+
+    frame.render_widget(String::from("Cursor: ") + &selected_byte.to_string(), status_bar_layout[0]);
+    frame.render_widget(String::from("Buffer: ") + input_buffer, status_bar_layout[1]);
 }
